14654d53d8f68d3607be10218f187c904c70cc9e
paranan Vitpornnitipacha <86040772+SariPV@users.noreply.github.com>
2022-03-21 23:13:26 +0700

    Update app.js

--- app.js
+++ app.js
@@ -29,7 +29,7 @@ app.use(cookieParser());
 
 
 // Plug routers
-app.use('/', indexRouter);
+app.use('/index', indexRouter);
 app.use('/users', usersRouter);
 app.use('/products', productsRouter);
 app.use('/customers',customerRouter);
